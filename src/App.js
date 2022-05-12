2682a6d69389b1b81caa1495b2db0c42cfbe23c3
AlaeddineAzri <alaeddinazri@gmail.com>
2022-05-12 23:41:41 +0100

    Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
+
 
 
 
@@ -9,7 +13,13 @@ export default function App() {
     <Router >
       <div className='flex flex-col justify-between h-screen'>
       <Navbar title="Github Tracker" />
-      <main className='container mx-auto px-3 pb-12'>main app </main>
+      <main className='container mx-auto px-3 pb-12'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+      </main>
       <Footer  />
       </div>
     </Router>
