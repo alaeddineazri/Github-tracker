8adaf842a7f71486f270fa60d59f6f04aef60d15
AlaeddineAzri <alaeddinazri@gmail.com>
2022-05-12 22:59:29 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
+
+
 export default function App() {
   return (
-    <div >
-    <h1 >
-      Hello world!
-    </h1>
-    </div>
+    <Router >
+      <div className='flex flex-col justify-between h-screen'>
+      <Navbar title="Github Tracker" />
+
+      <main>Content</main>
+      </div>
+    </Router>
   )
 }
